99858b053301de6bf06321a931973a6b9f0cd397
Ezekiel Abodesegun <ezekielphlat@gmail.com>
2022-11-13 19:07:54 +0100

    end split component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
+};
+const RightHandComponent = ({ message }) => {
+  return <p style={{ backgroundColor: "red" }}>{message}</p>;
+};
+
+function App() {
+  return (
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name="Ezekiel Abodesegun" />
+      <RightHandComponent message="This is just a message" />
+    </SplitScreen>
+  );
+}
+
+export default App;
